30c04e061575035ebeefa62523b098378d5b3b83
devdilip222-rgb <devdilip222@gmail.com>
2026-07-19 13:33:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
-BOT_TOKEN = ""
-API_ID =    
-API_HASH =""
+import os
+from os import environ
+
+API_ID = int(environ.get("API_ID", "22182189"))
+API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 ADMINS = []
 OWNER_ID = 
 LOG_CHANNEL =
-
-
-
-
