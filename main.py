db61c726f1a356166c66283320ac8f419f0a2a46
devdilip222-rgb <devdilip222@gmail.com>
2026-07-20 07:56:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ async def start_command(_, message: Message):
         "🔓 /glassmorphism → Glass Effect\n"
         "🔓 /cyberpunk → Neon Tech\n"
         "━━━━━━━━━━━━━━━━━━\n"
-        "👑 By: 『ᴀᴅᴍɪɴ』"
+        f"👑 By: {CREDIT}"
     )
 
 @client.on_message(filters.command("neumorphic") & filters.private)
